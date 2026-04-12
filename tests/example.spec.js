ff872fecee5431faff03e7883578d1429d5fbb4d
vigilanteX <dixitaman8@gmail.com>
2026-04-12 11:13:37 +0530

    pwgithubjenkins

---
+++ tests/example.spec.js
@@ -0,0 +1,13 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('print title', async ({ page }) => {
+  await page.goto('https://google.com/');
+  console.log(await page.title())
+
+});
+test('check title', async ({ page }) => {
+  await page.goto('https://google.com/');
+  await expect(page).toHaveTitle("Google")
+
+});
